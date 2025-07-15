ffba34c98650341fa526b309c325fd152f48ffcc
alex <alexyoung0119@gmail.com>
2025-07-14 22:26:22 -0500

    use new colors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased isolate`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased isolate bg-background`}
       >
         {children}
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <div className="isolate">
-      <h1>Hello world!</h1>
+    <div>
+      <h1 className="text-5xl font-bold text-primary">
+        hello worldington
+      </h1>
     </div>
   );
 }
